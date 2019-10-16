d860a79cd0738c276afe4c3a003606c818bb9cee
Sujay Anjankar <sanjankar@infocepts.com>
2019-10-16 17:15:49 +0800

    Custom component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import HeaderComponent from './HeaderComponent/HeaderComponent.js'
 import './App.css';
+import logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} className="App-logo" alt="logo" />
+        <HeaderComponent />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/HeaderComponent/HeaderComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './HeaderComponent.css'
+
+class HeaderComponent extends Component {
+    HeaderComponent(){}
+
+    render() {
+        return <h3 className='HeaderComp'> Header </h3>;
+    }
+}
+export default HeaderComponent;
