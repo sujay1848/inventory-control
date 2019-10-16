a20971caefeacfef14af49bfcde409450ecd0060
Sujay Anjankar <sanjankar@infocepts.com>
2019-10-16 17:54:12 +0800

    Added calls to internal function within component.

--- src/HeaderComponent/HeaderComponent.js
+++ src/HeaderComponent/HeaderComponent.js
@@ -5,7 +5,11 @@ class HeaderComponent extends Component {
     HeaderComponent(){}
 
     render() {
-        return <h3 className='HeaderComp'> Header </h3>;
+        return <h3 className='HeaderComp'> Header at {this.getTime()} </h3>;
+    }
+
+    getTime() {
+        return new Date().toDateString();
     }
 }
 export default HeaderComponent;
