96f44661298a15deebbf33bad5ffecd3a668e5c6
Sujay Anjankar <sanjankar@infocepts.com>
2019-10-18 16:02:58 +0800

    Fixed routing.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/inventory-control">
       <div>
         <Route exact path="/" component={HeaderPage} />
         <Route path="/start" component={StartPage} />
