77f0aa6a1bfe837a120938eba6a4a12a58b5e661
Sujay Anjankar <sujayanjankar@gmail.com>
2019-10-20 16:59:14 +0800

    SKU list table.

--- src/ScanPage/ScanPage.js
+++ src/ScanPage/ScanPage.js
@@ -3,7 +3,12 @@ import HeaderBar from '../HeaderBar/HeaderBar.js';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
-import '../HeaderPage/HeaderPage.css';
+import '../HeaderPage/HeaderPage.css'; import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
 
 export default class ScanPage extends Component {
 
@@ -12,9 +17,10 @@ export default class ScanPage extends Component {
         this.handleSkuIdChange = this.handleSkuIdChange.bind(this);
         this.handleSkuSave = this.handleSkuSave.bind(this);
         this.resetState = this.resetState.bind(this);
+        this.getSkuTable = this.getSkuTable.bind(this);
         this.state = {
             currentSku: '',
-            skuList: []
+            skuCountList: {}
         }
     }
 
@@ -25,17 +31,42 @@ export default class ScanPage extends Component {
     resetState() {
         this.setState({
             currentSku: '',
-            skuList: []
+            skuCountList: {}
         });
     }
 
     handleSkuSave(event) {
         var skuId = this.state.currentSku;
-        var localSkuList = this.state.skuList;
-        localSkuList.push(skuId);
-        this.setState({ skuList: localSkuList, currentSku: '' });
+        var skuList = this.state.skuCountList;
+        if (this.state.skuCountList[skuId]) {
+            skuList[skuId] = this.state.skuCountList[skuId] + 1;
+        } else if (skuId) {
+            skuList[skuId] = 1;
+        }
+        this.setState({ skuCountList: skuList, currentSku: '' });
     }
 
+    isScanDisabled() {
+        return !this.state.currentSku;
+    }
+
+    getSkuTable() {
+        if (this.state.skuCountList && Object.entries(this.state.skuCountList).length > 0) {
+            return (<Table>
+                <TableHead>
+                    <TableCell><Typography variant="h6">SKU ID</Typography></TableCell>
+                    <TableCell><Typography variant="h6">Count</Typography></TableCell>
+                </TableHead>
+                <TableBody>
+                    {Object.entries(this.state.skuCountList).map((keys) =>
+                        <TableRow>
+                            <TableCell>{keys[0]}</TableCell>
+                            <TableCell>{keys[1]}</TableCell>
+                        </TableRow>)}
+                </TableBody>
+            </Table>);
+        }
+    }
     render() {
         return (
             <div>
@@ -49,12 +80,11 @@ export default class ScanPage extends Component {
                         value={this.state.currentSku}
                         onChange={this.handleSkuIdChange}
                     />
-                    <p>SKUs scanned: {this.state.skuList.length}</p>
                     <div style={{ display: 'flex-vertical', flexGrow: 2 }}>
-                        <Button style={{ width: '100%' , marginBottom: 10 }} variant="contained" color="secondary" onClick={this.handleSkuSave}>
+                        <Button style={{ width: '100%', marginBottom: 10 }} variant="contained" color="secondary" disabled={this.isScanDisabled()} onClick={this.handleSkuSave}>
                             Scan
                         </Button>
-                        <Button style={{ width: '100%' , marginBottom: 10 }} variant="contained" color="secondary" onClick={this.resetState}>
+                        <Button style={{ width: '100%', marginBottom: 10 }} variant="contained" color="secondary" onClick={this.resetState}>
                             Start Again
                         </Button>
                         <Link to="/start" style={{ textDecoration: 'none' }}>
@@ -63,6 +93,7 @@ export default class ScanPage extends Component {
                             </Button>
                         </Link >
                     </div>
+                    <div>{this.getSkuTable()}</div>
                 </div>
             </div >
         );
