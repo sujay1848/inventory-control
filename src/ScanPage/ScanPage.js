19d08ecc33bc39be21f9ed0d4682c137593e5ab5
Sujay Anjankar <sanjankar@infocepts.com>
2019-10-21 13:58:41 +0800

    Scanner mode.

--- src/ScanPage/ScanPage.js
+++ src/ScanPage/ScanPage.js
@@ -9,6 +9,8 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Typography from '@material-ui/core/Typography';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 export default class ScanPage extends Component {
 
@@ -18,25 +20,50 @@ export default class ScanPage extends Component {
         this.handleSkuSave = this.handleSkuSave.bind(this);
         this.resetState = this.resetState.bind(this);
         this.getSkuTable = this.getSkuTable.bind(this);
+        this.handleSwitchToggle = this.handleSwitchToggle.bind(this);
+        this.handleSkuSaveWithId = this.handleSkuSaveWithId.bind(this);
         this.state = {
             currentSku: '',
+            scannerMode: false,
             skuCountList: {}
         }
     }
 
     handleSkuIdChange(event) {
-        this.setState({ currentSku: event.target.value });
+        this.setState({
+            currentSku: event.target.value,
+            skuCountList: this.state.skuCountList,
+            scannerMode: this.state.scannerMode
+        });
+        console.log('State before saving.' + this.state.currentSku)
+        console.log(this.state)
+        if (this.state.scannerMode) {
+            console.log('Change event for scanner mode.')
+            this.handleSkuSaveWithId(event.target.value);
+        }
+        console.log('State after saving.' + event.target.value)
+        console.log(this.state)
     }
 
     resetState() {
         this.setState({
             currentSku: '',
+            scannerMode: false,
             skuCountList: {}
         });
     }
 
-    handleSkuSave(event) {
-        var skuId = this.state.currentSku;
+    handleSwitchToggle() {
+        var previousScannerModeState = this.state.scannerMode;
+        this.setState({
+            currentSku: this.state.currentSku,
+            scannerMode: !previousScannerModeState,
+            skuCountList: this.state.skuCountList
+        });
+    }
+
+    handleSkuSaveWithId(skuId) {
+        console.log('Saving SKU.' + skuId)
         var skuList = this.state.skuCountList;
         if (this.state.skuCountList[skuId]) {
             skuList[skuId] = this.state.skuCountList[skuId] + 1;
@@ -44,22 +71,30 @@ export default class ScanPage extends Component {
             skuList[skuId] = 1;
         }
         this.setState({ skuCountList: skuList, currentSku: '' });
+        console.log('After saving SKU.');
+        console.log(this.state);
+    }
+
+    handleSkuSave() {
+        this.handleSkuSaveWithId(this.state.currentSku);
     }
 
     isScanDisabled() {
-        return !this.state.currentSku;
+        return !this.state.currentSku && this.state.scannerMode;
     }
 
     getSkuTable() {
         if (this.state.skuCountList && Object.entries(this.state.skuCountList).length > 0) {
-            return (<Table>
+            return (<Table stickyHeader >
                 <TableHead>
-                    <TableCell><Typography variant="h6">SKU ID</Typography></TableCell>
-                    <TableCell><Typography variant="h6">Count</Typography></TableCell>
+                    <TableRow>
+                        <TableCell><Typography variant="h6">SKU ID</Typography></TableCell>
+                        <TableCell><Typography variant="h6">Count</Typography></TableCell>
+                    </TableRow>
                 </TableHead>
                 <TableBody>
                     {Object.entries(this.state.skuCountList).map((keys) =>
-                        <TableRow>
+                        <TableRow key={keys[0]}>
                             <TableCell>{keys[0]}</TableCell>
                             <TableCell>{keys[1]}</TableCell>
                         </TableRow>)}
@@ -72,6 +107,14 @@ export default class ScanPage extends Component {
             <div>
                 <HeaderBar title="Inventory Control" />
                 <div className="container">
+                    <div style={{ width: '100%', display: 'flex', alignContent: 'flex-end' }}>
+                        <FormControlLabel
+                            control={
+                                <Switch onChange={this.handleSwitchToggle} checked={this.state.scannerMode} />
+                            }
+                            label="Scanner Mode"
+                        />
+                    </div>
                     <TextField
                         id="outlined-name"
                         label="CSKU ID"
