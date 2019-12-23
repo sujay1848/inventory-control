82d409fa80b7767d1cfad98a316ca48c5a4af4b4
junrong09 <junrongy09@gmail.com>
2019-12-23 16:07:09 +0800

    Add delete function in table

--- src/ReviewPage/EditableTable.js
+++ src/ReviewPage/EditableTable.js
@@ -4,6 +4,7 @@ import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
 import Table from "@material-ui/core/Table";
+import Bin from "../Img/delete.svg";
 
 export class EditableTable extends Component {
   constructor(props) {
@@ -19,6 +20,14 @@ export class EditableTable extends Component {
     this.props.setSkuCountList({ skuCountList: list });
   };
 
+  onRemoveSku = key => {
+    let list = Object.assign({}, this.props.skuCountList);
+    delete list[key];
+    this.props.setSkuCountList({ skuCountList: list });
+    // Overwrite the setting of selectedKey from TableRow onClick
+    setTimeout(() => this.setState({ selectedKey: null }), 50);
+  };
+
   isSelected = key => key === this.state.selectedKey;
 
   tableRow = data => {
@@ -32,25 +41,41 @@ export class EditableTable extends Component {
       >
         <TableCell align="center">{data[0]}</TableCell>
         <TableCell align="center">
-          <button
-            className={
-              "b--none bg-transparent f3 mr2" +
-              (this.isSelected(data[0]) ? "" : " hidden")
-            }
-            onClick={() => this.onCountChange(data, 1)}
-          >
-            +
-          </button>
-          {data[1]}
-          <button
-            className={
-              "b--none bg-transparent f3 ml2" +
-              (this.isSelected(data[0]) && data[1] > 1 ? "" : " hidden")
-            }
-            onClick={() => this.onCountChange(data, -1)}
-          >
-            -
-          </button>
+          <div className="flex justify-between items-center">
+            <div className=""></div>
+            <div className="flex items-center">
+              <button
+                className={
+                  "b--none bg-transparent f3 mr2" +
+                  (this.isSelected(data[0]) ? "" : " hidden")
+                }
+                onClick={() => this.onCountChange(data, 1)}
+              >
+                +
+              </button>
+              <span>{data[1]}</span>
+              <button
+                className={
+                  "b--none bg-transparent f3 ml2" +
+                  (this.isSelected(data[0]) && data[1] > 1 ? "" : " hidden")
+                }
+                onClick={() => this.onCountChange(data, -1)}
+              >
+                -
+              </button>
+            </div>
+            <div className="flex items-center">
+              <input
+                type="image"
+                src={Bin}
+                alt="bin"
+                onClick={() => this.onRemoveSku(data[0])}
+                className={
+                  "h1 w-auto" + (this.isSelected(data[0]) ? "" : " hidden")
+                }
+              />
+            </div>
+          </div>
         </TableCell>
       </TableRow>
     );
@@ -62,10 +87,10 @@ export class EditableTable extends Component {
         <TableHead>
           <TableRow>
             <TableCell align="center">
-              <span className="f5">SKU ID</span>
+              <span className="f5 b">SKU ID</span>
             </TableCell>
             <TableCell align="center">
-              <span className="f5">Count</span>
+              <span className="f5 b">Count</span>
             </TableCell>
           </TableRow>
         </TableHead>
